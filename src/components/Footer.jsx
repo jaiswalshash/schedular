48d892f79a0c1a3f31b4cd6cf27a5eb610dec0e7
Shashank Jaiswal <jaiswalshashank123@gmail.com>
2024-10-11 13:15:08 +0530

    UI updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navbar.css"
 
 const Footer = () => {
   return (
-    <footer className="bg-[#8C7147] text-white p-4 py-6 md:px-[4rem] lg:px-[4rem]">
+    <footer className="bg-[#8C7147] text-white p-4 py-6">
       {/* Welcome Message Section */}
-      <div className="text-center mb-4">
-        <p className="text-sm lg:text-base">
-          Welcome to our Ultimate Study Scheduler! Tailored for students preparing for competitive exams, our daily
-          schedules help maximize productivity and efficiency. Stay organized and focused with our expertly crafted study
-          plans and integrated resources. Start your journey to success today!
+      <div className="text-center flex justify-center mb-4">
+        <p className="text-lg max-w-5xl text-center flex">
+          Welcome to our Ultimate Study Scheduler! Tailored for students
+          preparing for competitive exams, our daily schedules help maximize
+          productivity and efficiency. Stay organized and focused with our
+          expertly crafted study plans and integrated resources. Start your
+          journey to success today!
         </p>
       </div>
-
+      <hr className="h-[2px] bg-opacity-50 w-full" />
       {/* Links Section */}
-      <div className="border-t border-black pt-4">
-        <ul className="flex justify-center space-x-8 text-sm">
+      <div className="pt-4">
+        <ul className="heading flex justify-center space-x-8 text-xl">
           <li>
             <Link to="/privacy-policy" className="hover:underline">
               PRIVACY POLICY
             </Link>
           </li>
           <li>
-
             <Link to="/contact" className="hover:underline">
               CONTACT US
             </Link>
           </li>
           <li>
-          <Link to="/about-us" className="hover:underline">
+            <Link to="/about-us" className="hover:underline">
               ABOUT US
             </Link>
           </li>
         </ul>
       </div>
+      <div style={{}} className="w-full mt-2 mb-0 flex justify-center text-sm items-center ">Designed by  © Vivek Verma</div>
     </footer>
   );
 };
